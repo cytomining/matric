88ae48c2964f679526b033c8ab7d3511a05ad441
Shantanu Singh <shsingh@broadinstitute.org>
2021-03-25 06:24:41 -0400

    init

---
+++ R/sim_calculate.R
@@ -0,0 +1,58 @@
+#' Calculate melted similarity matrix.
+#'
+#' \code{sim_calculate} calculates a melted similarity matrix.
+#'
+#' @param population tbl with annotations (a.k.a. metadata) and observation variables.
+#' @param annotation_prefix optional character string specifying prefix for annotation columns.
+#' @param method optional character string specifying method for \code{stats::cor} to calculate similarity.  This must be one of the strings \code{"pearson"} (default), \code{"kendall"}, \code{"spearman"}.
+#'
+#' @return data.frame of melted similarity matrix.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
+#'
+#' @examples
+#' suppressMessages(suppressWarnings(library(magrittr)))
+#' population <- tibble::tibble(
+#'   Metadata_group = sample(c("a", "b"), 4, replace = TRUE),
+#'   x = rnorm(4),
+#'   y = x + rnorm(4) / 100,
+#'   z = y + rnorm(4) / 1000
+#' )
+#' simplyr::sim_calculate(population, method = "pearson")
+#' @export
+sim_calculate <-
+  function(population,
+           annotation_prefix = "Metadata_",
+           method = "pearson") {
+    # get data matrix
+    data_matrix <-
+      population %>%
+      dplyr::select(-dplyr::matches(annotation_prefix))
+
+    # get metadata
+    metadata <-
+      population %>%
+      dplyr::select(dplyr::matches(annotation_prefix)) %>%
+      tibble::rowid_to_column(var = "id")
+
+    # measure similarities between treatments
+    stopifnot(method %in% c("pearson", "kendall", "spearman"))
+
+    sim_df <- stats::cor(t(data_matrix), method = method)
+
+    colnames(sim_df) <- seq(1, ncol(sim_df))
+
+    sim_df %<>%
+      tibble::as_tibble() %>%
+      tibble::rowid_to_column(var = "id1") %>%
+      tidyr::pivot_longer(-id1, names_to = "id2", values_to = "sim") %>%
+      dplyr::mutate(id2 = as.integer(id2)) %>%
+      dplyr::filter(id1 != id2)
+
+    attr(sim_df, "metric_metadata") <- list(method = method)
+
+    attr(sim_df, "row_metadata") <- metadata
+
+    sim_df
+  }
