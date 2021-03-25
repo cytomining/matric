cdf3a1f164724c5ef98e401d119e41fc05b4a99c
Shantanu Singh <shsingh@broadinstitute.org>
2021-03-25 06:45:17 -0400

    assignment pipe, tibble

---
+++ R/utils-assignment.R
@@ -0,0 +1,14 @@
+#' Assignment pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%<>\%}} for details.
+#'
+#' @name %<>%
+#' @rdname assignment-pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %<>%
+#' @usage lhs \%<>\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`.
+NULL
