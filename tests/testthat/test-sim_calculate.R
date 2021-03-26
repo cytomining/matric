f9f6a10433a2aaf55a096db737fbb24e852eb8ca
Shantanu Singh <shsingh@broadinstitute.org>
2021-03-25 21:05:28 -0400

    new test

--- tests/testthat/test-sim_calculate.R
+++ tests/testthat/test-sim_calculate.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("`sim_calculate` works", {
+  population <- tibble::tibble(
+    Metadata_group = sample(c("a", "b"), 4, replace = TRUE),
+    x = rnorm(4),
+    y = x + rnorm(4) / 100,
+    z = y + rnorm(4) / 1000
+  )
+  sim_df <- matric::sim_calculate(population, method = "pearson")
+  expect_equal(attr(sim_df, "row_metadata") %>% dplyr::select(-id),
+               population %>% dplyr::select(Metadata_group))
 })
