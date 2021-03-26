75cb527a7ecaa225ccce8d2366e828e6d89ac0ff
Shantanu Singh <shsingh@broadinstitute.org>
2021-03-26 08:37:40 -0400

    example

--- R/sim_collate.R
+++ R/sim_collate.R
@@ -83,6 +83,79 @@
 #' @param drop_group optional tbl; rows that match on \code{drop_group} on the left or right index are dropped.
 #'
 #' @return filtered \code{sim_df} with sets of pairs.
+#'
+#' @examples
+#'
+#' sim_df <- matric::sim_calculate(matric::cellhealth)
+#'
+#' drop_group <-
+#'   data.frame(Metadata_gene_name = "EMPTY")
+#'
+#' reference <-
+#'   data.frame(Metadata_gene_name = c("Chr2"))
+#'
+#' all_same_cols_ref <-
+#'   c("Metadata_cell_line",
+#'     "Metadata_Plate")
+#'
+#' all_same_cols_rep <-
+#'   c("Metadata_cell_line",
+#'     "Metadata_gene_name",
+#'     "Metadata_pert_name")
+#'
+#' all_same_cols_rep_ref <-
+#'   c(
+#'     "Metadata_cell_line",
+#'     "Metadata_gene_name",
+#'     "Metadata_pert_name",
+#'     "Metadata_Plate"
+#'   )
+#'
+#' any_different_cols_non_rep <-
+#'   c("Metadata_cell_line",
+#'     "Metadata_gene_name",
+#'     "Metadata_pert_name")
+#'
+#' all_same_cols_non_rep <-
+#'   c("Metadata_cell_line",
+#'     "Metadata_Plate")
+#'
+#' all_different_cols_non_rep <-
+#'   c("Metadata_gene_name")
+#'
+#' all_same_cols_group <-
+#'   c("Metadata_cell_line",
+#'     "Metadata_gene_name")
+#'
+#' any_different_cols_group <-
+#'   c("Metadata_cell_line",
+#'     "Metadata_gene_name",
+#'     "Metadata_pert_name")
+#'
+#' annotation_cols <-
+#'   c("Metadata_cell_line",
+#'     "Metadata_gene_name",
+#'     "Metadata_pert_name")
+#'
+#' collated_sim <-
+#'   matric::sim_collate(
+#'     sim_df,
+#'     reference,
+#'     all_same_cols_rep = all_same_cols_rep,
+#'     all_same_cols_rep_ref = all_same_cols_rep_ref,
+#'     all_same_cols_ref = all_same_cols_ref,
+#'     any_different_cols_non_rep = any_different_cols_non_rep,
+#'     all_same_cols_non_rep = all_same_cols_non_rep,
+#'     all_different_cols_non_rep = all_different_cols_non_rep,
+#'     any_different_cols_group = any_different_cols_group,
+#'     all_same_cols_group = all_same_cols_group,
+#'     annotation_cols = annotation_cols,
+#'     drop_group = drop_group
+#'   )
+#'
+#' head(collated_sim)
+#'
+#' collated_sim %>% dplyr::group_by(type) %>% dplyr::tally()
 #' @export
 #'
 sim_collate <-
