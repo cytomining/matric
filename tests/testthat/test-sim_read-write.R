14a0f944aba55f951f379e30dc8c27ee6ba08bb8
Shantanu Singh <shsingh@broadinstitute.org>
2021-03-26 07:22:11 -0400

    more tests

--- tests/testthat/test-sim_read-write.R
+++ tests/testthat/test-sim_read-write.R
@@ -1,10 +1,11 @@
 test_that("`sim_write` works", {
+  n <- 2
 
   population <- tibble::tibble(
-    Metadata_group = sample(c("a", "b"), 4, replace = TRUE),
-    x = rnorm(4),
-    y = x + rnorm(4) / 100,
-    z = y + rnorm(4) / 1000
+    Metadata_group = sample(c("a", "b"), n, replace = TRUE),
+    x = rnorm(n),
+    y = x + rnorm(n) / 100,
+    z = y + rnorm(n) / 1000
   )
 
   tmp_dir <- tempdir(check = TRUE)
@@ -45,16 +46,46 @@ test_that("`sim_write` works", {
     ignore_attr = TRUE
   )
 
-  expect_equal(metric_metadata$method,
-               jsonlite::read_json(json_file)$method[[1]])
+  expect_equal(
+    metric_metadata,
+    jsonlite::read_json(json_file,
+                        simplifyVector = TRUE))
 
   sim_file <- paste(tmp_path_prefix, "parquet", sep = ".")
 
   sim_df %>% matric::sim_write(sim_file)
   sim_df_in <- arrow::read_parquet(sim_file)
 
-  expect_equal(
-    sim_df,
-    sim_df_in
+  expect_equal(sim_df,
+               sim_df_in)
+})
+
+test_that("`sim_read` works", {
+
+  n <- 2
+
+  population <- tibble::tibble(
+    Metadata_group = sample(c("a", "b"), n, replace = TRUE),
+    x = rnorm(n),
+    y = x + rnorm(n) / 100,
+    z = y + rnorm(n) / 1000
   )
+
+  tmp_dir <- tempdir(check = TRUE)
+
+  tmp_path_prefix <- sprintf(file.path(tmp_dir, "test"))
+
+  sim_df <- sim_calculate(population, method = "pearson")
+  sim_df %>% sim_write(tmp_path_prefix, file_format = "csv")
+  sim_df_csv <- sim_read(tmp_path_prefix, file_format = "csv")
+
+  tmp_parquet_file <- paste(tmp_path_prefix, "parquet", sep = ".")
+
+  sim_df %>% sim_write(tmp_parquet_file)
+  sim_df_parquet1 <- sim_read(tmp_parquet_file)
+  sim_df %>% arrow::write_parquet(tmp_parquet_file)
+  sim_df_parquet2 <- arrow::read_parquet(tmp_parquet_file)
+
+  expect_equal(sim_df_parquet1, sim_df_parquet2)
+  expect_equal(sim_df_parquet1, sim_df_csv)
 })
