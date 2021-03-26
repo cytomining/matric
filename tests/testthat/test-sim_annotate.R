3f480eaf464d859a106be06fb417d305361fbe3c
Shantanu Singh <shsingh@broadinstitute.org>
2021-03-25 21:14:27 -0400

    new tests

---
+++ tests/testthat/test-sim_annotate.R
@@ -0,0 +1,16 @@
+test_that("`sim_annotate` works", {
+  population <- tibble::tibble(
+    Metadata_group = sample(c("a", "b"), 4, replace = TRUE),
+    Metadata_type = sample(c("x", "y"), 4, replace = TRUE),
+    x = rnorm(4),
+    y = x + rnorm(4) / 100,
+    z = y + rnorm(4) / 1000
+  )
+  annotation_cols <- c("Metadata_group")
+  # this is not a great test because it tests more than one function
+  sim_df <- matric::sim_calculate(population, method = "pearson")
+  sim_df <- matric::sim_annotate(sim_df, annotation_cols)
+
+  expect_equal(sort(unique(sim_df$Metadata_group1)),
+               sort(unique(sim_df$Metadata_group2)))
+})
