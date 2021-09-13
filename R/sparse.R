92afed419508a764d97b009ead65b0ced4a8c915
Shantanu Singh <shsingh@broadinstitute.org>
2021-09-13 06:49:55 -0400

    sparse uses furrr

--- R/sparse.R
+++ R/sparse.R
@@ -18,6 +18,14 @@
 #'   (and \code{id2}) containing the similarity between the pairs of rows
 #'   of \code{X}. \code{sim[i] == pairwise_function(X[id1[i], ], X[id2[i], ])}.
 sparse_pairwise <- function(X, id1, id2, pairwise_function, use_furrr = FALSE) {
+  if (use_furrr) {
+    logger::log_trace("Using furrr for parallel processing")
+    x_map2_dfr <- furrr::future_map2_dfr
+  } else {
+    logger::log_trace("Using purrr for sequential processing")
+    x_map2_dfr <- purrr::map2_dfr
+  }
+
   index_nest <-
     data.frame(id1, id2) %>%
     dplyr::arrange(id1, id2) %>%
@@ -27,29 +35,53 @@ sparse_pairwise <- function(X, id1, id2, pairwise_function, use_furrr = FALSE) {
     tidyr::nest(id1_l = c(id1))
 
   sim_df <-
-    purrr::map2_dfr(
+    x_map2_dfr(
       index_nest$id1_l,
       index_nest$id2_l,
-      function(l1, l2) {
-        index_sub <-
-          expand.grid(
-            id1 = l1[[1]],
-            id2 = l2[[1]],
-            KEEP.OUT.ATTRS = FALSE
-          )
-
-        S <-
-          as.vector(pairwise_function(X, l1[[1]], l2[[1]]))
-
-        index_sub <- index_sub %>% dplyr::mutate(sim = S)
-
-        index_sub
-      }
+      sparse_pairwise_helper,
+      X,
+      pairwise_function
     ) %>%
     dplyr::arrange(id1, id2)
 
   sim_df
 }
+
+
+#' Help compute similarity between pairs of rows of a matrix
+#'
+#' \code{sparse_pairwise} helps computes similarity between pairs of rows of a
+#'   matrix.
+#'
+#' @param X matrix
+#' @param idl1 list of integer vectors specifying the list of rows of \code{X}
+#'   (first set)
+#' @param idl2 list of integer vectors specifying the list of rows of \code{X},
+#'   (second set), same length as \code{idl1}.
+#' @param pairwise_function function that takes a matrix and a pair of indices
+#'   specifying rows of the matrix, and computes an operation of each pair of
+#'   rows
+#'
+#' @return data.frame with the same number of rows as the length of \code{idl1}
+#'   (and \code{id2}) containing the similarity between the pairs of rows
+#'   of \code{X}.
+#'   \code{sim[i] == pairwise_function(X[idl1[i], ], X[idl2[i], ])} where
+#'   idl1[i] and idl2[i] are each *lists* of indices.
+sparse_pairwise_helper <- function(idl1, idl2, X, pairwise_function) {
+        index_sub <-
+          expand.grid(
+            id1 = idl1[[1]],
+            id2 = idl2[[1]],
+            KEEP.OUT.ATTRS = FALSE
+          )
+
+        S <-
+          as.vector(pairwise_function(X, idl1[[1]], idl2[[1]]))
+
+        index_sub <- index_sub %>% dplyr::mutate(sim = S)
+
+        index_sub
+      }
 
 #' Compute cross product between two sets of rows of a matrix.
 #'
